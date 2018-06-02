b32b2f1ee649c6fd518dfc7672c5261d72add513
Felipe Cadavid <cadavid.felipe@hotmail.com>
2018-06-02 10:24:59 -0500

    Primer Commit

---
+++ src/reconocedorexpresiones/Calculadora.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reconocedorexpresiones;
+
+/**
+ *
+ * @author cadav
+ */
+public class Calculadora {
+    
+    public static double sumValues(int value1, int value2){
+        return value1 + value2;
+    }
+    
+    public static double substractValues(int value1, int value2){
+        return value1 - value2;
+    }
+    
+    public static double multiplyValues(int value1, int value2){
+        return value1 * value2;
+    }
+    
+    public static double divideValues(int value1, int value2){
+        return value1 / value2;
+    }
+}
